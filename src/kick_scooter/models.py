149d491ed42c5ca965b36db3fa92dff993176289
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-06-06 22:04:09 +0100

    Added card for products kick scooters along with creating model

--- src/kick_scooter/models.py
+++ src/kick_scooter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class KickScooters(models.Model):
+   title       = models.CharField(max_length=120)
+   stock       = models.IntegerField(blank=False)
+   feature_1   = models.CharField(max_length=220, null=True, blank=True)
+   feature_2   = models.CharField(max_length=220, null=True, blank=True)
+   feature_3   = models.CharField(max_length=220, null=True, blank=True)
+   content     = models.TextField(null=True, blank=True)
--- src/kick_scooter/views.py
+++ src/kick_scooter/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import KickScooters
 
 # Create your views here.
 def kick_scooter_page(request):
-   return render(request, 'kick_scooter.html', {})
+   qs = KickScooters.objects.all()
+   template_name = "kick_scooter.html"
+   context = {"kick_scooters_products": qs}
+   return render(request, template_name, context)
 
