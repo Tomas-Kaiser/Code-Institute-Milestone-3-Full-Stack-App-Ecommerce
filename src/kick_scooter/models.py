047f9c8d358f034e80d22fbdbd68dbbc84601d31
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-06-08 20:01:21 +0100

    Added price into models

--- src/kick_scooter/models.py
+++ src/kick_scooter/models.py
@@ -8,3 +8,4 @@ class KickScooters(models.Model):
    feature_2   = models.CharField(max_length=220, null=True, blank=True)
    feature_3   = models.CharField(max_length=220, null=True, blank=True)
    content     = models.TextField(null=True, blank=True)
+   price       = models.DecimalField(max_digits=6, decimal_places=2)
