0d813d17ab256159a08276813c1315eec7d11847
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-06-09 18:03:03 +0100

    Added eScooter page

--- src/products/urls.py
+++ src/products/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import (
     kick_scooter_page,
     kick_scooter_detail,
+    eScooter_page,
 )
 
 urlpatterns = [
-    path('kick-scooters/', kick_scooter_page, name="kick-scooter"),
+    path('kick-scooters/', kick_scooter_page, name="kick-scooter-products"),
+    path('eScooters/', eScooter_page, name="eScooter-products"),
     path('kick-scooters/<str:slug>', kick_scooter_detail),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
+# Render only kick scooters
 def kick_scooter_page(request):
    qs = Product.objects.filter(slug__icontains='kick-scooter')
-   template_name = "kick_scooter.html"
+   template_name = "kick_scooters.html"
    context = {"kick_scooters_products": qs}
    return render(request, template_name, context)
 
@@ -12,4 +13,11 @@ def kick_scooter_detail(request, slug):
    qs = get_object_or_404(Product, slug=slug)
    template_name = "kick_scooter_detail.html"
    context = {"kick_scooter_detail": qs}
+   return render(request, template_name, context)
+
+# Render only eScooters
+def eScooter_page(request):
+   qs = Product.objects.filter(slug__icontains='eScooter')
+   template_name = "eScooters.html"
+   context = {"kick_scooters_products": qs}
    return render(request, template_name, context)
