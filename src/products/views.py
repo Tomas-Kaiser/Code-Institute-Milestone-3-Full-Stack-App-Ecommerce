918d4ef7c0014e68e316755295a9c60e80bce0ba
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-06-09 17:25:10 +0100

    Added views

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Product
+
+def kick_scooter_page(request):
+   qs = Product.objects.all()
+   template_name = "kick_scooter.html"
+   context = {"kick_scooters_products": qs}
+   return render(request, template_name, context)
+
+def kick_scooter_detail(request, slug):
+   qs = get_object_or_404(Product, slug=slug)
+   template_name = "kick_scooter_detail.html"
+   context = {"kick_scooter_detail": qs}
+   return render(request, template_name, context)
