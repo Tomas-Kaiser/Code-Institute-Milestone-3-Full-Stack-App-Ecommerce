d5259806e95199628d8270b504c062efdcd05cdc
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-06-09 18:12:58 +0100

    Added kid_scooter page

--- src/products/urls.py
+++ src/products/urls.py
@@ -4,10 +4,12 @@ from .views import (
     kick_scooter_page,
     kick_scooter_detail,
     eScooter_page,
+    kid_scooter_page,
 )
 
 urlpatterns = [
     path('kick-scooters/', kick_scooter_page, name="kick-scooter-products"),
     path('eScooters/', eScooter_page, name="eScooter-products"),
+    path('kid-scooters/', kid_scooter_page, name="kid-scooter-products"),
     path('kick-scooters/<str:slug>', kick_scooter_detail),
 ]
