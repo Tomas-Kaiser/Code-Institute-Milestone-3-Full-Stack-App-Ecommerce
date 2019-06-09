3a14cd1a9eeb4228066bebe718d2e2336091c02d
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-06-09 22:03:37 +0100

    Created a folder for kick scooter

--- src/products/urls.py
+++ src/products/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('kick-scooters/', kick_scooter_page, name="kick-scooter-products"),
     path('eScooters/', eScooter_page, name="eScooter-products"),
     path('kid-scooters/', kid_scooter_page, name="kid-scooter-products"),
-    path('kick-scooters/<str:slug>', kick_scooter_detail),
+    path('kick-scooters/<str:slug>', kick_scooter_detail),   
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -5,14 +5,14 @@ from .models import Product
 # Render only kick scooters
 def kick_scooter_page(request):
    qs = Product.objects.filter(slug__icontains='kick-scooter')
-   template_name = "kick_scooters.html"
+   template_name = "kick_scooter/kick_scooters.html"
    context = {"kick_scooters_products": qs}
    return render(request, template_name, context)
 
 def kick_scooter_detail(request, slug):
    qs = get_object_or_404(Product, slug=slug)
    print(qs)
-   template_name = "kick_scooter_detail.html"
+   template_name = "kick_scooter/kick_scooter_detail.html"
    context = {"kick_scooter_detail": qs}
    return render(request, template_name, context)
 
