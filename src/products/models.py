12877fcae9aa6477f6c989e4e55bb775365c7ca5
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-06-10 22:15:43 +0100

    Kept consistent indentation

--- src/products/models.py
+++ src/products/models.py
@@ -11,8 +11,8 @@ class Product(models.Model):
    price       = models.DecimalField(max_digits=6, decimal_places=2)
 
    def __str__(self):
-        return self.title
+          return self.title
 
    def get_absolute_url(self):
-       return f"{self.slug}"
+          return f"{self.slug}"
    
