b6516c6e979ff6dba4ca00eb756e197f48dd8ac8
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-07-06 20:05:29 +0100

    Moved loging.html to templates in accounts app

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 
 from .views import (
    register_page,
+   login_page,
    profile,
 )
 
 urlpatterns = [
+   path('login/', login_page, name="login"),
    path('', include('django.contrib.auth.urls')),
    path('register/', register_page, name="register"),
    path('profile/', profile, name="profile"),
