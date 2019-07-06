7ac508241d18ae8daeb29f599eb19a9be97b809e
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-07-06 19:36:52 +0100

    Added style into login

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -170,3 +170,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
