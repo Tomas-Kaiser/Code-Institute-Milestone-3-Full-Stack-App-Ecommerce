18d2622620485048d6d5df98dfd9726d43c5e49a
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-07-11 21:19:15 +0100

    Added animation when adding the product into shipping cart

---
+++ src/Kick Scooters _ eCommerce - Kaiser_files/cart.js.download
@@ -0,0 +1,19 @@
+$(window).on("scroll", function () {
+   if ($(window).scrollTop() > 38) {
+       $('#cart').css({
+          "position" : "fixed",
+          "top" : "50px",
+          "right" : "30px",
+          "z-index" : "1001",
+          "border" : "1.5px solid green",
+          "border-radius" : "14px",
+         });
+   } else {
+       $('#cart').css({
+          "position" : "absolute",
+          "top" : "9px",
+          "right" : "140px",
+          "border" : "",
+         });
+   }
+});
--- src/static/js/cart.js
+++ src/static/js/cart.js
@@ -1,19 +1,29 @@
 $(window).on("scroll", function () {
+   // fixed the shopping card when scrolling down
    if ($(window).scrollTop() > 38) {
-       $('#cart').css({
-          "position" : "fixed",
-          "top" : "50px",
-          "right" : "30px",
-          "z-index" : "1001",
-          "border" : "1.5px solid green",
-          "border-radius" : "14px",
-         });
+      $('#cart').css({
+         "position": "fixed",
+         "top": "50px",
+         "right": "30px",
+         "z-index": "1001",
+         "border": "1.5px solid green",
+         "border-radius": "14px",
+      });
    } else {
-       $('#cart').css({
-          "position" : "absolute",
-          "top" : "9px",
-          "right" : "140px",
-          "border" : "",
-         });
+      $('#cart').css({
+         "position": "absolute",
+         "top": "9px",
+         "right": "140px",
+         "border": "",
+      });
    }
+});
+
+// Animation when adding a new product into shopping cart
+$("button").on("click", function () {
+   $(".badge").addClass("zoom")
+
+   setTimeout(function () {
+      $(".badge").removeClass("zoom");
+   }, 1000);
 });
