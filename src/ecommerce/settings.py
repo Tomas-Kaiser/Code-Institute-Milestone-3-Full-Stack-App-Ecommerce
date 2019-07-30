3cc8d55d3e9eeb6c8c6f42d11520f0b9cf5a4e0b
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-07-30 21:10:14 +0100

    Added  AWS default acl to None

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -153,6 +153,8 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
+AWS_DEFAULT_ACL = None
+
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStrorage'
 
